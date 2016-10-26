e928d84efde26a96012ffde0b960aba11795d239
Oksana <pasom@bigmir.net>
2016-10-27 02:28:58 +0400

    Threads

---
+++ src/com/threads/rerunThreads/NewThread.java
@@ -0,0 +1,40 @@
+package com.threads.rerunThreads;
+/*
+312
+ */
+public class NewThread implements Runnable{
+    String name; //thread name
+    Thread t;
+    boolean suspendFlag;
+
+    NewThread(String name){
+        this.name = name;
+        t = new Thread(this, name);
+        System.out.println("new thread: " + t);
+        suspendFlag = false;
+        t.start();
+    }
+    public void run(){
+        try {
+            for (int i = 15; i > 0; i--){
+                System.out.println(name + ": " + i);
+                Thread.sleep(2000);
+                synchronized (this){
+                    while(suspendFlag){
+                        wait();
+                    }
+                }
+            }
+        }catch (InterruptedException e){
+            System.out.println("interruption :( ");
+        }
+        System.out.println(name + " is finished");
+    }
+    synchronized void mysuspend(){
+        suspendFlag = true;
+    }
+    synchronized void myresume(){
+        suspendFlag = false;
+        notify();
+    }
+}
---
+++ src/com/threads/rerunThreads/SuspendResume.java
@@ -0,0 +1,31 @@
+package com.threads.rerunThreads;
+
+public class SuspendResume {
+    public static void main(String[] args) {
+        NewThread ob1 = new NewThread("one");
+        NewThread ob2 = new NewThread("two");
+        try {
+            Thread.sleep(1000);
+            ob1.mysuspend();
+            System.out.println("delay thread one");
+            Thread.sleep(1000);
+            ob1.myresume();
+            System.out.println("rerun thread one");
+            ob2.mysuspend();
+            System.out.println("delay thread two");
+            Thread.sleep(1000);
+            ob2.myresume();
+            System.out.println("rerun thread two");
+        }catch (InterruptedException e){
+            System.out.println("main thread have been interrupted");
+        }
+        try {
+            System.out.println("waiting to threads finish work");
+            ob1.t.join();
+            ob2.t.join();
+        }catch (InterruptedException e){
+            System.out.println("main thread have been interrupted");
+        }
+        System.out.println("main thread have been finished");
+    }
+}
