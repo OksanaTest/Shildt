5a8ff7821c2a47b15abebcb48d9ac656c2671284
Oksana <pasom@bigmir.net>
2016-10-27 00:11:55 +0400

    Threads

---
+++ src/com/threads/synchronisedBlockWorkDemo/CallMe.java
@@ -0,0 +1,13 @@
+package com.threads.synchronisedBlockWorkDemo;
+
+public class CallMe {
+    void call(String msg){
+        System.out.print("[ " + msg);
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            System.out.println("interruption");
+        }
+        System.out.println("]");
+    }
+}
