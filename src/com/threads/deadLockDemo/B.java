cae3caf1db0e280e441c859f2209c3851cbc3a9d
Oksana <pasom@bigmir.net>
2016-10-27 01:43:09 +0400

    Threads

---
+++ src/com/threads/deadLockDemo/B.java
@@ -0,0 +1,20 @@
+package com.threads.deadLockDemo;
+/*
+309
+ */
+public class B {
+    synchronized void bar(A a){
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " come into method B.bar()");
+        try {
+            Thread.sleep(1000);
+        }catch (Exception e){
+            System.out.println("Class is interrupted");
+        }
+        System.out.println(name + " try to call method A.last()");
+        a.last();
+    }
+    synchronized void last(){
+        System.out.println("in the method A.last()");
+    }
+}
