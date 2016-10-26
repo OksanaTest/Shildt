3cf7a8da40041322a3b79a09cf416f4c79b2282c
Oksana <pasom@bigmir.net>
2016-10-26 06:55:34 +0400

    Threads

---
+++ src/com/threads/NewThredsRunnable/NewThread.java
@@ -0,0 +1,25 @@
+package com.threads.NewThredsRunnable;
+/*
+295
+ */
+public class NewThread implements Runnable{
+    String name; //thread name
+    Thread t;
+    NewThread(String name){
+        this.name = name;
+        t = new Thread(this, name);
+        System.out.println("new thread: " + t);
+        t.start();
+    }
+    public void run(){
+        try {
+            for (int i = 5; i > 0; i--){
+                System.out.println(name + ": " + i);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e){
+            System.out.println("new thread was interrupted");
+        }
+        System.out.println(name + " was finished");
+    }
+}
