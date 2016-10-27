d360d1faa077c0b673c9a330ed0015de8e9dca5a
Oksana <pasom@bigmir.net>
2016-10-27 16:46:23 +0400

    Threads

---
+++ src/com/threads/exchangerWorkDemo/ExgrDemo.java
@@ -0,0 +1,14 @@
+package com.threads.exchangerWorkDemo;
+/*
+1020
+ */
+import java.util.concurrent.Exchanger;
+
+public class ExgrDemo {
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<>();
+        new UseString(exchanger);
+        new MakeString(exchanger);
+
+    }
+}
---
+++ src/com/threads/exchangerWorkDemo/UseString.java
@@ -0,0 +1,25 @@
+package com.threads.exchangerWorkDemo;
+
+import java.util.concurrent.Exchanger;
+
+public class UseString implements Runnable {
+    Exchanger<String> stringExchanger;
+    String string;
+
+    UseString(Exchanger<String> stringExchanger) {
+        this.stringExchanger = stringExchanger;
+        new Thread(this).start();
+    }
+
+    public void run() {
+        for (int i = 0; i < 3; i ++) {
+            try {
+                //change empty buffer to full
+                string = stringExchanger.exchange(new String());
+                System.out.println("receive: " + string);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
