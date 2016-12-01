9786d9ff4d4eac725f3035763b79e05aed0a5667
Oksana <pasom@bigmir.net>
2016-12-01 08:14:44 +0300

    RemoveBlochEx

---
+++ src/com/arrays/TwoDArray.java
@@ -0,0 +1,15 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+
+/*
+98
+ */
+public class TwoDArray {
+    public static void main(String[ ] s) {
+
+
+        System.out.println(s.length);
+    }
+}
